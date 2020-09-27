07b7faa65b4b01ea3b53608c05012d6d738ba55f
Dinesh Sadhwani <dineshsadhwani4@gmail.com>
2020-09-28 01:54:07 +0530

    populated pizza page and added toppings buttons

--- src/pages/pizzas.js
+++ src/pages/pizzas.js
@@ -1,9 +1,41 @@
+import { graphql } from 'gatsby';
 import React from 'react';
+import PizzaList from '../components/Pizza';
+import ToppingsFilter from '../components/ToppingsFilter';
 
-export default function PizzasPage() {
+export default function PizzasPage({ data: { pizzas } }) {
   return (
     <>
-      <h1>Hi There 👋 I'm the Pizzas Page </h1>
+      <ToppingsFilter />
+      <PizzaList pizzas={pizzas.nodes} />
     </>
   );
 }
+
+export const query = graphql`
+  query PizzaQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fixed(width: 200, height: 200) {
+              ...GatsbySanityImageFixed
+            }
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
